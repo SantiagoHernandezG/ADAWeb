b8cf5d5bb62c7f8d4eb2d69ac5949b20a5405909
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-03-21 17:20:22 -0600

    Creacion env y config env

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Uso de librerias para ejecutar express js y poder leer req.body de los inputs
 const express = require("express");
+const dotenv = require("dotenv");
 var path = require('path');
 const app = express();
 var bodyParser = require("body-parser");
@@ -8,34 +9,49 @@ app.set('view engine', 'ejs');
 var assert = require('assert');
 var fs = require('fs');
 require('dotenv/config');
+dotenv.config({path: './config/config.env'})
 app.use(bodyParser.urlencoded({ extender: true }));
+//Monogoose para usar base de datos
+const mongoose = require("mongoose");
+//Conexion a base de datos en MongoDB a traves de URL del cluster
+mongoose.connect("mongodb+srv://Demian:57008345@cluster0.twupy.mongodb.net/ADA?retryWrites=true&w=majority", { useNewUrlParser: true })
 
 
 app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
 
+
+const PORT = process.env.PORT || 5000
+
 //Inicio de servidor en puerto default
-app.listen(3000, function () {
-	console.log("Servidor corriendo en el puerto 3000");
-    
-})
+app.listen(
+	PORT,
+	console.log(`Servidor corriendo en el modo de: ${process.env.NODE_ENV} en el peurto ${PORT}`)
+)
+//Schema para los usuarios
+const UsuarioSchema = {
+	nombre: String,
+	correo: String,
+	contrasena: String
+}
+
 //Ir a pagina de inicio
 app.get("/", function (req, res) {
 	res.render('index')
 	
 })
-/* Redireccionamiento de las demas vistas*/
-app.get("/instructivo", function (req, res) {
-	res.render('instructivo')
+/* Redireccionamiento de las demas vistas (Modulos del sistema)*/
+app.get("/perfil", function (req, res) {
+	res.render('perfil')
 	
 })
-app.get("/metodologia", function (req, res) {
-	res.render('metodologia')
+app.get("/conferencias", function (req, res) {
+	res.render('conferencias')
 })
-app.get("/matriz", function (req, res) {
-	res.render('matriz')
+app.get("/eventos", function (req, res) {
+	res.render('eventos')
 })
-app.get("/calculos", function (req, res) {
-	res.render('calculos')
+app.get("/nosotrxs", function (req, res) {
+	res.render('nosotrxs')
 })
