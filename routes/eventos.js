444381fcb9a26a3cadf0e9e19224ef84b3542b3c
Archana <A01335895@itesm.mx>
2022-03-27 20:18:15 -0600

    Added Detalle evento

--- routes/eventos.js
+++ routes/eventos.js
@@ -4,5 +4,9 @@ const router = express.Router()
 router.get("/", function (req, res) {
 	res.render('eventos')
 })
+router.get("/evento", function (req, res) {
+	res.render('eventoDetail')
+})
+
 
 module.exports = router
