7d82c686edf03cc9c3584f3cd46e467f1e66f5d9
pedrodemiangodinezcruz <30833341+pedrodemiangodinezcruz@users.noreply.github.com>
2022-05-10 20:51:54 -0500

    Conferencias v2.0

--- models/eventos.js
+++ models/eventos.js
@@ -1,4 +1,4 @@
-//Schema para trabajador
+//Schema para eventos
 const EventoSchema = {
 	nombre: String,
 	nombrePresentador: String,
@@ -7,26 +7,4 @@ const EventoSchema = {
 	fotoEvento: String,
 	descripcion: String,
 }
-//Altas evento
-app.post("/evento", function (req, res) {
-	
-	var cipher = crypto.createCipher(algorithm, key); 
-	var codigo = req.body.codigo;
-	console.log(codigo);
 
-	let newTrabajador = new trabajador({
-		nombre: req.body.Nombre,
-		apellido: req.body.Apellido,
-		correo: req.body.Email,
-		contrasena: cipher.update(req.body.contrasenia, 'utf8', 'hex') + cipher.final('hex'),
-		tipo: req.body.Profesion
-	});
-	res.render('Eventos', { alta: 'true' })
-	newTrabajador.save();
-	console.log("Alta de evento exitosa");
-}
-	
-	else{
-		res.render('Registro', { alta: 'false' })
-	}
-})
--- routes/conferencias.js
+++ routes/conferencias.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require("mongoose");
+var path = require('path');
+const app = express();
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({ extender: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 router.get("/", function (req, res) {
-	res.render('conferencias')
+	res.render('conferencias', { alta: '' })
 })
 router.get("/conferencia", function (req, res) {
 	res.render('conferenciaDetail')
@@ -11,5 +19,44 @@ router.get("/conferencia", function (req, res) {
 $('.datepicker').datepicker({
 	inline: true
   });*/
+
+//Schema para trabajador
+const ConferenciaSchema = {
+	nombre: String,
+	nombrePresentador: String,
+	fecha: Date,
+	contacto: String,
+	fotoEvento: String,
+	descripcion: String,
+}
+const conferencia = mongoose.model('conferencias', ConferenciaSchema);
+//Altas evento
+app.post("/altaConferencia", function (req, res) {
+	console.log("LLefgue aqui");
+	/*var cipher = crypto.createCipher(algorithm, key); 
+	var codigo = req.body.codigo;
+	console.log(codigo);*/
+
+	let newConferencia = new conferencia({
+		nombre: req.body.nombre,
+		nombrePresentador: req.body.nombrePresentador,
+		fecha: req.body.trip-start,
+		contacto: req.body.email,
+		promocional: req.body.promocional,
+		descripcion: req.body.descripcion		
+	});
+
+	try{
+	res.render('conferencias', { alta: 'true' })
+	newConferencia.save();
+	console.log("Alta de conferencia exitosa");
+	}
+	catch (error) {
+		res.render('conferencias', { alta: 'false' })
+		console.error(error);
+		console.log("Alta no exitosa");
+	}
+})
+
 
 module.exports = router;
