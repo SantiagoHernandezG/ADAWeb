613c2eb021cb3f101bb5e514a20481637c694f54
SantiagoHernandezG <Santiagohernandez2@icloud.com>
2022-05-12 23:39:18 -0500

    Tests setup

---
+++ tests/test.js
@@ -0,0 +1,27 @@
+/*
+let logic = require('../services/calcu.js')
+
+var assert = require('assert');
+describe('Calculadora', function () {
+    describe('#suma()', function () {
+        it('Debe regresar 22', function () {
+            assert.equal(logic.suma([10,12]), 22);
+        });
+    });
+    describe('#resta()', function () {
+        it('Debe regresar -2', function () {
+            assert.equal(logic.resta([10,12]), -2);
+        });
+    });
+    describe('#multiplicacion()', function () {
+        it('Debe regresar 15', function () {
+            assert.equal(logic.multiplicacion([5,3]), 15);
+        });
+    });
+    describe('#division()', function () {
+        it('Debe regresar 2', function () {
+            assert.equal(logic.division([12,6]), 2);
+        });
+    });
+});
+*/
